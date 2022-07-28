4a3f8c86e12f78be04845890db37cead38394e5a
priamcse <priamcse@outlook.com>
2022-07-28 18:21:33 +0600

    react-router, helmet installed + header footer done

---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src="../../logo.svg" alt="Prothom Alo" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "./App.scss";
+import Theme from "./components/layouts/Theme";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>
-)
+);
