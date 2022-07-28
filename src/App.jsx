03887aa9afab75e637211f8cc71061e56ce8605a
priamcse <priamcse@outlook.com>
2022-07-28 18:34:33 +0600

    react-router-dom done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 // import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App";
 import "./index.css";
 import "./App.scss";
 import Theme from "./components/layouts/Theme";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Theme>
-      <App />
-    </Theme>
+    <BrowserRouter>
+      <Theme>
+        <App />
+      </Theme>
+    </BrowserRouter>
   </React.StrictMode>
 );
